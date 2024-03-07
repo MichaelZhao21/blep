1e5c98c7a7478b0ff475f0e5700d27acaccb0c4b
Michael Zhao <michaelzhao314@gmail.com>
2024-03-07 01:54:40 -0600

    Modified text on home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="center">
+	<h1>:P</h1>
+	<a href="/wanted">make a wanted poster :)</a>
+</div>
+
+<style>
+    .center {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        height: 100%;
+        width: 100%;
+    }
+</style>
